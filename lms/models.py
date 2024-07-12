fd8ce46a1f6884083db9ecf5abb3535ef1d321e4
Alexander <a.biryukov164@gmail.com>
2024-07-12 20:42:04 +0400

    Описал CRUD для моделей Course и Lesson

--- lms/models.py
+++ lms/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
-# Create your models here.
+from config.settings import NULLABLE
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название курса')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(verbose_name='Превью', **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+    def __str__(self):
+        return self.name
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название урока')
+    description = models.TextField(verbose_name='Описание', **NULLABLE)
+    preview = models.ImageField(verbose_name='Превью', **NULLABLE)
+    course = models.ForeignKey(Course, verbose_name='Курс', on_delete=models.SET_NULL, **NULLABLE)
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
+
+    def __str__(self):
+        return self.name
