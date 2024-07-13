1cefda61ed728d38cb2730a21d98632339a835c6
Alexander <a.biryukov164@gmail.com>
2024-07-13 23:08:46 +0400

    Для сериализатора для модели курса реализовал поле вывода уроков

--- lms/serializer.py
+++ lms/serializer.py
@@ -4,18 +4,19 @@ from rest_framework.serializers import ModelSerializer
 from lms.models import Course, Lesson
 
 
-class CourseSerializer(ModelSerializer):
-    lesson_count = SerializerMethodField()
-
-    class Meta:
-        model = Course
-        fields = ['id', 'name', 'description', 'lesson_count']
-
-    def get_lesson_count(self, course):
-        return Lesson.objects.filter(course_id=course.id).count()
-
-
 class LessonSerializer(ModelSerializer):
     class Meta:
         model = Lesson
         fields = '__all__'
+
+
+class CourseSerializer(ModelSerializer):
+    lesson_count = SerializerMethodField()
+    lessons = LessonSerializer(source='lesson_set', many=True)
+
+    class Meta:
+        model = Course
+        fields = '__all__'
+
+    def get_lesson_count(self, course):
+        return Lesson.objects.filter(course_id=course.id).count()
