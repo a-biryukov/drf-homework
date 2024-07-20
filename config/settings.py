8aa8767e7ddbf1154b6f15f531cde03a52334f3e
Alexander <a.biryukov164@gmail.com>
2024-07-20 13:18:37 +0400

    установил и настроил библиотеку djangorestframework-simplejwt

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 import os
+from datetime import timedelta
 from pathlib import Path
 from dotenv import load_dotenv
 
@@ -41,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'djangorestframework-simplejwt',
     'rest_framework',
     'django_filters',
 
@@ -141,6 +143,20 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
 NULLABLE = {"blank": True, "null": True}
+
+REST_FRAMEWORK = {
+    'DEFAULT_FILTER_BACKENDS': (
+        'django_filters.rest_framework.DjangoFilterBackend'
+    ),
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication'
+    )
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=15),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+}
 
 COUNTRIES_ONLY = {
     'RU': 'Россия',
--- users/urls.py
+++ users/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
 from users.views import PaymentsListAPIView, UserUpdateAPIView
@@ -7,5 +8,7 @@ app_name = UsersConfig.name
 
 urlpatterns = [
     path('<int:pk>/update/', UserUpdateAPIView.as_view()),
-    path('payments/', PaymentsListAPIView.as_view())
+    path('payments/', PaymentsListAPIView.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
