75b482629c9f798c360daffa8a372ec1359ea61d
Alexander <a.biryukov164@gmail.com>
2024-07-21 16:31:19 +0400

    настроил получение списка платежей по владельцу

--- users/views.py
+++ users/views.py
@@ -45,8 +45,10 @@ class UserDestroyAPIView(DestroyAPIView):
 
 
 class PaymentsListAPIView(ListAPIView):
-    queryset = Payments.objects.all()
     serializer_class = PaymentsSerializer
     filter_backends = [DjangoFilterBackend, OrderingFilter]
     filterset_fields = ('paid_course', 'paid_lesson', 'payment_method')
     ordering_fields = ('date_of_payment', )
+
+    def get_queryset(self):
+        return Payments.objects.filter(user=self.request.user)
