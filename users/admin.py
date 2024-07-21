fe9c3ba41094a39ab9e1e1820c0255aa7c8b4bdf
Alexander <a.biryukov164@gmail.com>
2024-07-21 16:20:40 +0400

    настроил отображение платежей в админ-панели

--- users/admin.py
+++ users/admin.py
@@ -1,8 +1,13 @@
 from django.contrib import admin
 
-from users.models import User
+from users.models import User, Payments
 
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'email', 'avatar', 'country',)
+
+
+@admin.register(Payments)
+class PaymentsAdmin(admin.ModelAdmin):
+    list_display = ('id', 'user', 'paid_course', 'paid_lesson', 'date_of_payment', 'payment_amount', 'payment_method')
