db4f58908a01e10e6ece9597492b0e2e0049702e
Alexander <a.biryukov164@gmail.com>
2024-07-24 23:22:28 +0400

    реализовал валидацию материалов на ссылки

--- lms/serializer.py
+++ lms/serializer.py
@@ -1,10 +1,14 @@
 from rest_framework.fields import SerializerMethodField
-from rest_framework.serializers import ModelSerializer
+from rest_framework.serializers import ModelSerializer, CharField
 
 from lms.models import Course, Lesson
+from lms.validators import validate_urls
 
 
 class LessonSerializer(ModelSerializer):
+    name = CharField(validators=[validate_urls])
+    description = CharField(validators=[validate_urls])
+
     class Meta:
         model = Lesson
         fields = '__all__'
@@ -13,6 +17,8 @@ class LessonSerializer(ModelSerializer):
 class CourseSerializer(ModelSerializer):
     lesson_count = SerializerMethodField()
     lessons = LessonSerializer(source='lesson_set', many=True, read_only=True)
+    name = CharField(validators=[validate_urls])
+    description = CharField(validators=[validate_urls])
 
     class Meta:
         model = Course
