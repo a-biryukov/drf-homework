6e28259c050e22a7d0f90b9c7bd66995cc10c4c1
Alexander <a.biryukov164@gmail.com>
2024-07-25 14:13:25 +0400

    реализовал пагинацию для вывода всех уроков и курсов

--- lms/views.py
+++ lms/views.py
@@ -1,9 +1,9 @@
 from rest_framework.generics import CreateAPIView, ListAPIView, RetrieveAPIView, UpdateAPIView, DestroyAPIView
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 
 from lms.models import Course, Lesson
+from lms.paginators import CustomPaginator
 from lms.permissions import IsModer, IsOwner
 from lms.serializer import CourseSerializer, LessonSerializer
 
@@ -11,14 +11,17 @@ from lms.serializer import CourseSerializer, LessonSerializer
 class CourseViewSet(ModelViewSet):
     queryset = Course.objects.all()
     serializer_class = CourseSerializer
+    pagination_class = CustomPaginator
 
     def list(self, request, *args, **kwargs):
         if IsModer().has_permission(self.request, self):
             queryset = Course.objects.all()
         else:
             queryset = Course.objects.filter(owner=request.user)
-        serializer = CourseSerializer(queryset, many=True)
-        return Response(serializer.data)
+
+        page = self.paginate_queryset(queryset.order_by('id'))
+        serializer = CourseSerializer(page, many=True)
+        return self.get_paginated_response(serializer.data)
 
     def perform_create(self, serializer):
         course = serializer.save()
@@ -48,12 +51,13 @@ class LessonCreateAPIView(CreateAPIView):
 
 class LessonListAPIView(ListAPIView):
     serializer_class = LessonSerializer
+    pagination_class = CustomPaginator
 
     def get_queryset(self):
         if IsModer().has_permission(self.request, self):
-            return Lesson.objects.all()
+            return Lesson.objects.all().order_by('id')
         else:
-            return Lesson.objects.filter(owner=self.request.user)
+            return Lesson.objects.filter(owner=self.request.user).order_by('id')
 
 
 class LessonRetrieveAPIView(RetrieveAPIView):
